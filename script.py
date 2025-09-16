3dd419ea28d59890e991bb8b8e0f6d52d3380da7
Yannik Zeyer <yannik.zeyer@fmc-ag.com>
2025-09-16 15:52:24 +0200

    jobrad works

--- script.py
+++ script.py
@@ -7,18 +7,46 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from acora import AcoraBuilder
 from selenium.common.exceptions import TimeoutException
+import datetime
+import os
+
+def handleJobPosting(title, link):
+    if(len(ac.findall(title)) != 0):
+        posting = {
+            "title" : title,
+            "link" : link,
+            "date" : datetime.date.today()
+        }
+        if(not oldpostings or not any(x['link']==posting['link'] for x in oldpostings['jobs'])):     
+            newpostings['jobs'].append(posting)
+    
+recentpostings = dict()
+if(os.stat("data/newjobs.json").st_size != 0):
+    with open('data/newjobs.json') as json_file:
+        recentpostings = json.load(json_file)
+
+oldpostings = dict()
+if(os.stat("data/oldjobs.json").st_size != 0):
+    with open('data/oldjobs.json') as json_file:
+        oldpostings = json.load(json_file)
+
+if(oldpostings):
+    if(len(recentpostings['jobs']) != 0):
+        oldpostings['jobs'].extend(recentpostings['jobs'])
+else:
+    oldpostings = recentpostings
 
 keywords = []
 with open("data/dictionary") as file:
     keywords = [line.rstrip() for line in file]
-print(keywords)
 builder = AcoraBuilder(keywords)
 ac = builder.build()
+
 companies = dict()
 with open('data/inputs.json') as json_file:
     companies = json.load(json_file)
-    print(companies['companies'][0])
 
+newpostings = { "jobs" : []}
 
 #use selenium headless to search inputurls 
 chrome_options = Options()
@@ -37,14 +65,18 @@ for i in companies['companies']:
             print("Internet too slow")
         try:
             postings = driver.find_elements(By.CSS_SELECTOR, 'a.result-item')
-            print(len(postings))
             for job in postings:
                 title = job.find_element(By.CLASS_NAME, 'title')
-                if(len(ac.findall(title.get_property('title'))) != 0):
-                   print(title.get_property('title'))
-                   print(job.get_attribute('href'))
+                handleJobPosting(title.get_property('title'),job.get_attribute('href'))
         except NoSuchElementException:
             print("No current postings jor Jobrad")
 driver.quit()
+with open('data/newjobs.json', 'w') as json_file:
+    json.dump(newpostings, json_file, default=str)
+with open('data/oldjobs.json', 'w') as json_file:
+    json.dump(oldpostings, json_file, default=str)
+#WRITE POSTINGS IN FILE json format
+#WRITE OLD POSTINGS IN FILE old postings
+
 # write wrappers for every single inputurls because gthese sites differ
 # wanted output is job postings in data structure with url, jobname, posted date and locations
